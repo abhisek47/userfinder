94ff2bc8f45704294ea753810dde3cf5582404e1
abhisek47 <abhisek529@gmail.com>
2020-05-10 15:00:36 +0530

    Updates

---
+++ precache-manifest.5f092f055c658a540162b103c8198f86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a23cb1413d190746700d9699f7022fde",
+    "url": "/userfinder/index.html"
+  },
+  {
+    "revision": "e66b1f6e4f94cdd70362",
+    "url": "/userfinder/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "ec1d6f69e3c8d9579a7c",
+    "url": "/userfinder/static/css/main.fca6b21e.chunk.css"
+  },
+  {
+    "revision": "e66b1f6e4f94cdd70362",
+    "url": "/userfinder/static/js/2.a07facc1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/userfinder/static/js/2.a07facc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec1d6f69e3c8d9579a7c",
+    "url": "/userfinder/static/js/main.44602eac.chunk.js"
+  },
+  {
+    "revision": "71afe0c0cf8955c73104",
+    "url": "/userfinder/static/js/runtime-main.1009d979.js"
+  }
+]);
